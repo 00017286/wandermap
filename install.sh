0c14b0fe75644dff582da736753aeea2c3fcd664
00017286 <polinadolgopolova@gmail.com>
2025-03-20 22:54:14 +0300

    Updated

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ set -e
 
 echo "Начало установки ODBC-драйверов..."
 
-# Устанавливаем зависимости
-sudo apt-get update && sudo apt-get install -y curl unixodbc-dev
+# Устанавливаем зависимости (без sudo)
+apt-get update --allow-releaseinfo-change && apt-get install -y curl unixodbc-dev
 
-# Скачиваем и устанавливаем Microsoft ODBC Driver 17 для SQL Server
-curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://packages.microsoft.com/debian/10/prod buster main" | sudo tee /etc/apt/sources.list.d/mssql-release.list
-sudo apt-get update && sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
+# Скачиваем и устанавливаем Microsoft ODBC Driver 17 для SQL Server (без sudo)
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+echo "deb [arch=amd64] https://packages.microsoft.com/debian/10/prod buster main" > /etc/apt/sources.list.d/mssql-release.list
+apt-get update && ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
 # Создаем директорию для конфигурации ODBC
 mkdir -p ~/.odbcinst
